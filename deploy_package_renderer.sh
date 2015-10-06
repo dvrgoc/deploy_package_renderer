1550a540c7bc616aca85827fdf8170131ab0bdef
dna <write2danijel@gmail.com>
2015-10-07 00:07:25 +0200

    checkout to specified new_commit_id

--- deploy_package_renderer.sh
+++ deploy_package_renderer.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 clear
 
-# load configurable options are loaded from terminal
+# configurable options are loaded from terminal
 GIT_PROJECT_ROOT=$1
 OLDER_COMMIT_ID=$2
 NEWER_COMMIT_ID=$3
-
+# this is scripts output folder
 INIT_FOLDER=$HOME'/Desktop/deploy_package'
+
 rm -rf $INIT_FOLDER
 mkdir $INIT_FOLDER
 
 # change directory to to your project specified in $1
 cd $GIT_PROJECT_ROOT
 
+# get current branch name, will be used at the end of the script
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# checkout to specified commit id, ie, go back in history
+git checkout $NEWER_COMMIT_ID | >> /dev/null
+
+# get the list of files that were modified between the two commits
 git diff --name-only $OLDER_COMMIT_ID $NEWER_COMMIT_ID | while read LINE
 do
 	# output the file that was committed
-	echo 'GIT file: '$LINE
+	echo 'file in GIT repo: '$LINE
 	# explode string to obtain folder structure
 	ARRAY=$(echo $LINE | tr "/" "\n")
 	FULL_PATH=''
@@ -39,5 +47,7 @@ do
 	done
 	echo '----'
 done
+# move back the head of the branch to where it was
+git checkout $BRANCH
 echo 'The deploy package renderer script is finished.'
 echo 'https://github.com/dvrgoc thanks you for using it. :)'
